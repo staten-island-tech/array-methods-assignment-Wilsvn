c0dc3aa1ed2d51ed61888b854657c1864a105fd9
Wilsvn <wilsonvposcablo@gmail.com>
2022-11-03 10:16:46 -0400

    w

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const DogsArr = [(Johnny = { name: "Lab", type: "retriever" })];
-console.log(DogsArr);
+const DogsArr = [
+  { Name: "Johnny", Breed: "Labrador", Type: "Retriever", Numbers: [1, 2, 3] },
+  { Name: "Rocky", Breed: "Golden Retriver", Type: "Retriever", Numbers: [2, 4, 6] },
+  { Name: "Sunny", Breed: "Chesapeake", Type: "Retriever", Numbers: [3, 6, 9] },
+  { Name: "Rex", Breed: "Pitbull", Type: "Terrier", Numbers: [4, 5, 6] },
+  {Name: "Bone", Breed:"Schnauzer", Type: "Terrier", Numbers: [5, 6, 7] },
+  {Name: "Aurora", Breed: "Airedale", Type: "Terrier", Numbers: [6, 7, 8]}
+];
+DogsArr.forEach((DogsArr.Name), => console.log(DogsArr.Name));
+
