77b11ea9a559f3ad3947ba156315a94d9c1699fb
Wilsvn <wilsonvposcablo@gmail.com>
2022-11-04 09:43:56 -0400

    w

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const DogsArr = [
   { Name: "Johnny", Breed: "Labrador", Type: "Retriever", Numbers: [1, 2, 3] },
-  { Name: "Rocky", Breed: "Golden Retriver", Type: "Retriever", Numbers: [2, 4, 6] },
+  {
+    Name: "Rocky",
+    Breed: "Golden Retriver",
+    Type: "Retriever",
+    Numbers: [2, 4, 6],
+  },
   { Name: "Sunny", Breed: "Chesapeake", Type: "Retriever", Numbers: [3, 6, 9] },
   { Name: "Rex", Breed: "Pitbull", Type: "Terrier", Numbers: [4, 5, 6] },
-  {Name: "Bone", Breed:"Schnauzer", Type: "Terrier", Numbers: [5, 6, 7] },
-  {Name: "Aurora", Breed: "Airedale", Type: "Terrier", Numbers: [6, 7, 8]}
+  { Name: "Bone", Breed: "Schnauzer", Type: "Terrier", Numbers: [5, 6, 7] },
+  { Name: "Aurora", Breed: "Airedale", Type: "Terrier", Numbers: [6, 7, 8] },
 ];
-DogsArr.forEach((DogsArr.Name), => console.log(DogsArr.Name));
+DogsArr.forEach((DogsArr) => console.log(DogsArr.Name));
+DogsArr.forEach((DogsArr) => console.log(DogsArr.Numbers));
+const result = DogsArr.filter((DogsArr) => DogsArr.Type === "Terrier");
 
+console.log(result);
